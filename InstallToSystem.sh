fa0319058c6f076ae1014ef94831e281ac16bd6f
Leah <rlfx@realfx.rocks>
2024-09-25 16:03:56 +0000

    [install] init install script

---
+++ InstallToSystem.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# setup
+
+mkdir -p ~/.local/bin
+
+# The colons around PATH is to get compatibility with paths on the start or end of the path var.
+if [[ ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
+  echo "[+] Your path contains ~/.local/bin."
+else
+  echo "[!] Your path is missing ~/.local/bin, you might want to add it."
+fi
+
+# sysup
+
+ln -sf $PWD/sysup $HOME/.local/bin/sysup
+
+if [ $? -eq 0 ]; then
+  echo "[+] sysup successfully linked."
+else
+  echo "[!] sysup failed to be linked!"
+fi
+
+# End
+exit
+
